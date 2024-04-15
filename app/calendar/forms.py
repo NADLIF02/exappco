01df49fc0a0bd7124d01ab4edc6dd2c167dad70a
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 11:48:41 +0200

    Create forms.py

---
+++ app/calendar/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField
+from wtforms.validators import DataRequired
+
+class EventForm(FlaskForm):
+    event_date = DateField('Event Date', format='%Y-%m-%d', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Add Event')
