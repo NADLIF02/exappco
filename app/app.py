e73da229a7b452dabe0c24f15541626cb04b06ec
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 13:41:00 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-from flask import render_template, redirect, url_for, session
-from app.event_calendar.forms import EventForm
+from flask import Flask, render_template, request, redirect, url_for, session
+from event_calendar.forms import EventForm  # Adjusted import
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
@@ -7,15 +8,15 @@ app.secret_key = 'your_secret_key'
 def home():
     if not session.get('logged_in'):
         return redirect(url_for('auth.login'))
-    form = EventForm()  # Créez une instance du formulaire
+    form = EventForm()  # Create an instance of the form
     return render_template('event_calendar/calendar.html', form=form)
-    
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        # Ajouter la logique de vérification ici
+        # Verification logic here
         session['logged_in'] = True
         return redirect(url_for('home'))
     return render_template('auth/login.html')
