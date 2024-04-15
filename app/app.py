c53a28720f1c2eaa9e186283f30db42e80125f50
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 13:21:21 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-
+from flask import render_template, redirect, url_for, session
+from .event_calendar.forms import EventForm 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def home():
     if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    else:
-        return render_template('event_calendar/calendar.html')
-
+        return redirect(url_for('auth.login'))
+    form = EventForm()  # Créez une instance du formulaire
+    return render_template('event_calendar/calendar.html', form=form)
+    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
