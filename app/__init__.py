2cd8b625022b1250ebaaea8e848d1cc1881681ba
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 15:34:26 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,12 @@ from .event_calendar import event_calendar as calendar_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.default')  # Assuming you have a config directory with settings
+    app.config['SECRET_KEY'] = 'your_secret_key'
 
+    from app.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from app.event_calendar import event_calendar as calendar_blueprint
     app.register_blueprint(calendar_blueprint, url_prefix='/calendar')
 
     return app
