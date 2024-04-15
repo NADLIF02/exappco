397441c10e85cf7a49dae2e84280e165f8f0bca8
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 16:06:53 +0200

    Update __init__.py

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,6 @@
+# app/auth/__init__.py
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
-from . import routes  # Relative import of routes within the same Blueprint package
+from . import routes
