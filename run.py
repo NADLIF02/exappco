f05b570cda0da1a37303f1d18aff540c7a564dad
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 16:33:01 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
 from app import create_app
+import logging
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://sys.stdout',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = create_app()
 
