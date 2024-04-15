5839a35e407a0c68c6a2818e6cc3a2be081fba7e
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 14:24:02 +0200

    Update __init__.py

--- app/event_calendar/__init__.py
+++ app/event_calendar/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
-from .views import new_leave, view_calendar
 
-bp = Blueprint('event_calendar', __name__, url_prefix='/event_calendar')
-bp.add_url_rule('/new', view_func=new_leave, methods=['GET', 'POST'])
-bp.add_url_rule('/view', view_func=view_calendar, methods=['GET'])
+event_calendar = Blueprint('event_calendar', __name__)
+
+from . import routes
