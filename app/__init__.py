9a57e8a98386e1203e2eef691b4a70c68da617a9
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 16:19:14 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,10 @@ def create_app():
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-    # Delay import until after Blueprint creation to avoid circular imports
     from .event_calendar import event_calendar as calendar_blueprint
     app.register_blueprint(calendar_blueprint, url_prefix='/calendar')
+
+    # Import routes after the blueprint registration
+    from .event_calendar import routes  # This import is deferred until after the Blueprint is registered
 
     return app
