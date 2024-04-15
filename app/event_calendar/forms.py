de4090db77e8a09b947ab0bee04f67a4601934fa
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 14:02:18 +0200

    Update forms.py

--- app/event_calendar/forms.py
+++ app/event_calendar/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SubmitField
+from wtforms import StringField, DateField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
-class EventForm(FlaskForm):
-    event_date = DateField('Event Date', format='%Y-%m-%d', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
-    submit = SubmitField('Add Event')
+class LeaveRequestForm(FlaskForm):
+    employee_name = StringField('Employee Name', validators=[DataRequired()])
+    start_date = DateField('Start Date', format='%Y-%m-%d', validators=[DataRequired()])
+    end_date = DateField('End Date', format='%Y-%m-%d', validators=[DataRequired()])
+    reason = TextAreaField('Reason for Leave')
+    submit = SubmitField('Submit Leave Request')
