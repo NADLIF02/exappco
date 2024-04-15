28c719074a17fe592d182eed44a5362ae03ce1ab
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 14:23:38 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()  # Create the SQLAlchemy object
+from .event_calendar import event_calendar
+from .auth import auth
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'
     app.config['SECRET_KEY'] = 'your_secret_key'
-    
-    db.init_app(app)  # Initialize db with the Flask app
 
-    from app.event_calendar import bp as calendar_bp
-    app.register_blueprint(calendar_bp)
+    app.register_blueprint(auth)
+    app.register_blueprint(event_calendar)
 
     return app
