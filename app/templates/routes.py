123f5fd663e9ba56c76fa89bbfbadb063dbe8f67
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 11:32:05 +0200

    Create routes.py

---
+++ app/templates/routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, request, redirect, url_for, session
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    if not session.get('logged_in'):
+        return redirect(url_for('.login'))
+    else:
+        return render_template('calendar.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Ajouter la logique de vérification ici
+        session['logged_in'] = True
+        return redirect(url_for('.home'))
+    return render_template('login.html')
