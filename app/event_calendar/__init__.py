73b67c96b7462a9ad7876f31fa5db591410c972b
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 16:18:30 +0200

    Update __init__.py

--- app/event_calendar/__init__.py
+++ app/event_calendar/__init__.py
@@ -1,6 +1,5 @@
-# app/event_calendar/__init__.py
 from flask import Blueprint
 
 event_calendar = Blueprint('event_calendar', __name__)
 
-from . import routes
+# Do not import routes here to avoid circular import
