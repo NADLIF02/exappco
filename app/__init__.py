43fa5b5516b2082f7d08bbdde816e7120fdbaa73
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 16:23:43 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,13 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
 
+    # Import the blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    # Import the event_calendar Blueprint and its routes beforehand
     from .event_calendar import event_calendar as calendar_blueprint
+    from .event_calendar import routes  # Import routes before registering the blueprint
     app.register_blueprint(calendar_blueprint, url_prefix='/calendar')
-
-    # Import routes after the blueprint registration
-    from .event_calendar import routes  # This import is deferred until after the Blueprint is registered
 
     return app
