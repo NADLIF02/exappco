caf2f63bce4b02f99d8902e3f893039323b69914
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-15 11:47:42 +0200

    Update views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,16 @@
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, request, redirect, url_for, session, flash
 from . import auth
+from .forms import LoginForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # Logique de vérification des identifiants
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        # Logique de vérification des identifiants ici
+        # Supposons une vérification réussie :
         session['logged_in'] = True
+        flash('You have been logged in.', 'success')
         return redirect(url_for('calendar.display'))
-    return render_template('auth/login.html')
+    return render_template('auth/login.html', form=form)
